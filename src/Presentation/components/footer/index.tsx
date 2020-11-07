67c54146dd4614261ea98eb45c5468a5bfaf66e2
Henrique Leao <henrique.s3.leao@gmail.com>
2020-11-06 21:30:05 -0300

    refactor: move footer to separated file

---
+++ src/Presentation/components/footer/index.tsx
@@ -0,0 +1,13 @@
+import React, { memo } from 'react';
+
+import './style.scss';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      footer
+    </footer>
+  )
+};
+
+export default memo(Footer);

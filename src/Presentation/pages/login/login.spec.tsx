33956b31bcbdedfc935ba54796e1da5c0b1466a6
Henrique Leao <henrique.s3.leao@gmail.com>
2020-11-07 10:55:33 -0300

    refactor: move sut creation to a factory method

--- src/Presentation/pages/login/login.spec.tsx
+++ src/Presentation/pages/login/login.spec.tsx
@@ -2,21 +2,26 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Login from './index';
 
+const makeSut = () => {
+  const sut = render(<Login />);
+  return { sut };
+}
+
 describe('Login Component', () => {
   it('Should not render spinner and error on start', () => {
-    render(<Login />);
+    makeSut();
     const formStatus = screen.getByRole('formStatus');
     expect(formStatus.childElementCount).toBe(0);
   });
 
   it('Should not render button with enable state', () => {
-    render(<Login />);
+    makeSut();
     const submitButton = screen.getByRole('button') as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
   });
 
   it('Should render all inputs with madatory filling', () => {
-    render(<Login />);
+    makeSut();
     const inputEmail = screen.getByRole('email');
     expect(inputEmail.title).toBe('Campo obrigatório');
     expect(inputEmail.textContent).toBe('🔴');

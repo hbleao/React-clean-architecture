9fb070cb15dec0fc7ad7b5c06d234c00c9d76332
Henrique Leao <henrique.s3.leao@gmail.com>
2020-11-07 10:45:50 -0300

    test: ensure Login start with inputs required state

--- src/Presentation/pages/login/login.spec.tsx
+++ src/Presentation/pages/login/login.spec.tsx
@@ -5,10 +5,25 @@ import Login from './index';
 describe('Login Component', () => {
   it('Should not render spinner and error on start', () => {
     render(<Login />);
-    const formStatus = screen.getByRole('login-formStatus');
+    const formStatus = screen.getByRole('formStatus');
     expect(formStatus.childElementCount).toBe(0);
+  });
 
-    const submitButton = screen.getByRole('login-button') as HTMLButtonElement;
+  it('Should not render button with enable state', () => {
+    render(<Login />);
+    const submitButton = screen.getByRole('button') as HTMLButtonElement;
     expect(submitButton.disabled).toBe(true);
-  })
+  });
+
+  it('Should render all inputs with madatory filling', () => {
+    render(<Login />);
+    const inputEmail = screen.getByRole('email');
+    expect(inputEmail.title).toBe('Campo obrigatório');
+    expect(inputEmail.textContent).toBe('🔴');
+    const inputPassword = screen.getByRole('password');
+    expect(inputPassword.title).toBe('Campo obrigatório');
+    expect(inputPassword.textContent).toBe('🔴');
+  });
+
+
 })

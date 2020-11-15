a350320fdde57a5fce40565a8499dcb35d35d312
Henrique Leao <henrique.s3.leao@gmail.com>
2020-11-15 20:10:42 -0300

    feat: move SUT creation to a factory method

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -2,15 +2,17 @@ import faker from 'faker';
 import { InvalidFieldError } from "@/validation/errors";
 import { EmailValidation } from "./email-validation";
 
+const makeSut = () => new EmailValidation(faker.database.column());
+
 describe('EmailValidation', () => {
   it('Should return error if email is invalid', () => {
-    const sut = new EmailValidation(faker.random.word());
+    const sut = makeSut();
     const error = sut.validate(faker.random.word());
     expect(error).toEqual(new InvalidFieldError());
   });
 
   it('Should return error if email is valid', () => {
-    const sut = new EmailValidation(faker.random.word());
+    const sut = makeSut();
     const error = sut.validate(faker.internet.email());
     expect(error).toBeFalsy();
   });
